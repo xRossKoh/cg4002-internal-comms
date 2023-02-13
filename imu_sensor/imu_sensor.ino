3cd7b8c5cc974230fabbf404a90b08915ad274af
Kenneth <kkmk1999@gmail.com>
2023-02-13 20:20:03 +0700

    Added Arduino IDE sketches for IR and IMU sensors (dummy data)

---
+++ imu_sensor/imu_sensor.ino
@@ -0,0 +1,164 @@
+#pragma pack(1)
+
+enum PacketType
+{
+  HELLO,
+  ACK,
+  NACK,
+  DATA
+};
+
+typedef struct
+{
+  uint8_t header;           // contains beetle id and packet type
+  uint8_t padding;          // Padding header to 2 bytes
+  int euler_x;              // contains IR data for data packet for IR sensors
+  int euler_y;              // all other fields padded with 0 for data packet for IR sensors
+  int euler_z;
+  int acc_x;
+  int acc_y;
+  int acc_z;
+  uint16_t crc;             // Cyclic redundancy check (CRC-16)
+} BLEPacket; 
+
+const unsigned int MAX_MESSAGE_LENGTH = 16;
+
+uint8_t serial_buffer[MAX_MESSAGE_LENGTH];
+BLEPacket* curr_packet;
+
+bool is_connected = false;
+
+BLEPacket default_packets[3];
+
+uint16_t crcCalc(uint8_t* data)
+{
+   uint16_t curr_crc = 0x0000;
+   uint8_t sum1 = (uint8_t) curr_crc;
+   uint8_t sum2 = (uint8_t) (curr_crc >> 8);
+   int index;
+   for(index = 0; index < MAX_MESSAGE_LENGTH; index = index+1)
+   {
+      sum1 = (sum1 + data[index]) % 255;
+      sum2 = (sum2 + sum1) % 255;
+   }
+   return (sum2 << 8) | sum1;
+}
+
+bool crcCheck()
+{
+  uint16_t crc = curr_packet->crc;
+  curr_packet->crc = 0;
+  return (crc == crcCalc((uint8_t*)curr_packet));
+}
+
+bool packetCheck(uint8_t node_id, PacketType packet_type)
+{
+  uint8_t header = curr_packet->header;
+  uint8_t curr_node_id = (header & 0xf0) >> 4;
+  PacketType curr_packet_type = PacketType(header & 0xf);
+  return curr_node_id == node_id && curr_packet_type == packet_type;
+}
+
+void waitForData()
+{
+  unsigned int buf_pos = 0;
+  while (buf_pos < MAX_MESSAGE_LENGTH)
+  {
+    if (Serial.available())
+    {
+      uint8_t in_data = Serial.read();
+      serial_buffer[buf_pos] = in_data;
+      buf_pos++;
+    }
+  }
+  curr_packet = (BLEPacket*)serial_buffer;
+}
+
+BLEPacket generatePacket(PacketType packet_type, int* data)
+{
+  BLEPacket p;
+  p.header = (3 << 4) | packet_type;
+  p.padding = 0;
+  p.euler_x = data[0];
+  p.euler_y = data[1];
+  p.euler_z = data[2];
+  p.acc_x = data[3];
+  p.acc_y = data[4];
+  p.acc_z = data[5];
+  p.crc = 0;
+  uint16_t calculatedCRC = crcCalc((uint8_t*)&p);
+  p.crc = calculatedCRC;
+  return p;
+}
+
+void generateDefaultPackets()
+{
+  int data[] = {0, 0, 0, 0, 0, 0};
+  for (int i = 0; i < 3; i++)
+  {
+//    incorrect casting
+//    BLEPacket p = generatePacket(PacketType(i), data);
+//    default_packets[i] = (byte*)&p;
+    default_packets[i] = generatePacket(PacketType(i), data);
+  }
+}
+
+void sendDefaultPacket(PacketType packet_type)
+{
+  Serial.write((byte*)&default_packets[packet_type], MAX_MESSAGE_LENGTH);
+}
+
+void sendDataPacket(int* data)
+{
+  BLEPacket p = generatePacket(DATA, data);
+  Serial.write((byte*)&p, MAX_MESSAGE_LENGTH);
+}
+
+void threeWayHandshake()
+{
+  while (!is_connected)
+  {
+    // wait for hello from laptop
+    waitForData();
+  
+    if (!crcCheck() or !packetCheck(0, HELLO))
+    {
+      sendDefaultPacket(NACK);
+      continue;
+    } 
+    sendDefaultPacket(HELLO);
+    
+    // wait for ack from laptop
+    waitForData();
+    
+    if (crcCheck() && packetCheck(0, ACK))
+    {
+      is_connected = true;
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  generateDefaultPackets();
+  threeWayHandshake();
+}
+
+void loop() {
+  delay(20);
+  int data[6];
+  for (int i = 0; i < 6; i++)
+  {
+    data[i] = random(-100, 100); 
+  }
+  sendDataPacket(data);
+//  bool is_ack = false;
+//  while (!is_ack) // packet will keep sending until it is acknowledged by laptop
+//  {
+//    sendDataPacket(data);
+//    if (crcCheck() && packetCheck(ACK))
+//    {
+//      is_ack = true;
+//    }
+//  }
+}
