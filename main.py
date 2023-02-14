d96fbf8bd01f154eb32ef9bac6897163829806a6
Kenneth <kkmk1999@gmail.com>
2023-02-14 17:50:38 +0700

    Added MAC address to main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from controller import Controller
 
-controller = Controller((1, ""), (2, ""), (3, ""))
+controller = Controller((1, "b0:b1:13:2d:d3:79"), (2, "b0:b1:13:2d:d6:37"), (3, "c4:be:84:20:1b:09"))
 controller.run_threads()
