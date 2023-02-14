8bc66238f503371cc625ad68363cae7e9c91c65b
Kenneth <kkmk1999@gmail.com>
2023-02-14 12:15:06 +0700

    Added Scheduler class in scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -2,10 +2,21 @@ from bluno_beetle import BlunoBeetle
 
 class Scheduler:
     def __init__(self, params):
-        self.beetle_ids = []
-        self.mac_addrs = []
+        self.beetles = []
         for param in params:
-            self.beetle_ids.append(param[0])
-            self.mac_addrs.append(param[1])
+            self.beetles.append(BlunoBeetle(param))
+
+    def connect():
+        for beetle in self.beetles:
+            beetle.connect()
 
     def main(self):
+        while True:
+            for beetle in self.beetles:
+                beetle.wait_for_data()
+
+scheduler = Scheduler([
+    (1, "b0:b1:13:2d:b6:3d"),   // ir_transmitter 
+    (2, "b0:b1:13:2d:d6:37"),   // ir_receiver
+    (3, "c4:be:84:20:1b:09")    // imu_sensor
+])
