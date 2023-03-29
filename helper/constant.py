3d82ad6d308ff4b8479fc8b825d310a3bae56d22
Kenneth <kkmk1999@gmail.com>
2023-03-29 11:00:44 +0700

    changed MAC address for p1 and p2 suits

--- helper/constant.py
+++ helper/constant.py
@@ -23,12 +23,12 @@ PACKET_TYPE_POS = 2
 # MAC addresses of Bluno Beetles
 # Player 1
 P1_IR_TRANSMITTER = "b0:b1:13:2d:cb:8c"
-P1_IR_RECEIVER = "c4:be:84:20:1b:09"
+P1_IR_RECEIVER = "b0:b1:13:2d:b6:3d"
 P1_IMU_SENSOR = "c4:be:84:20:1a:51"
 
 # Player 2
 P2_IR_TRANSMITTER = "b0:b1:13:2d:d3:79"
-P2_IR_RECEIVER = "b0:b1:13:2d:b6:3d"
+P2_IR_RECEIVER = "c4:be:84:20:1b:09"
 P2_IMU_SENSOR = "c4:be:84:20:19:4c"
 
 # Length of strings printed to std O/P
